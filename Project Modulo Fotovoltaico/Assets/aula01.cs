7ac7d806b9cb2b404e466fecbe27d0164cc2b051
MoisesMLP <moisesmlp@hotmail.com>
2021-08-12 11:01:10 -0300

    Adicionando o projeto no unity

---
+++ Project Modulo Fotovoltaico/Assets/aula01.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aula01 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("OLA MUNDO...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
